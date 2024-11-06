befc99c9aabbced521772abae81c4f2eca47f034
Dzemal <136269036+Thevaultboy11@users.noreply.github.com>
2024-11-06 17:25:02 +0100

    Switching To New Repository

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import CardList from './CardList';
+function App() {
+  return (
+    <main>
+        <div className="App">
+          <CardList/>
+        </div>
+    </main>
+   
+  );
+}
+
+export default App;
